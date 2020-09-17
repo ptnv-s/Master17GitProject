024d6827e53a8d4cb333110ad38d5054f7522861
Raymond Liang <raymond.liang96@gmail.com>
2020-09-17 14:37:46 +0100

    Added table to people.html and associated JS.

--- src/people.js
+++ src/people.js
@@ -4,7 +4,20 @@ const people = [
     {"name": "Mo", "age": 31},
 ];
 
+
+
 for (const person of people) {
     // Build a table (using the dom) and add the people above
+    let table = document.getElementById('people-table');
+    let row = document.createElement('tr');
+    let nameCol = document.createElement('td');
+    let ageCol = document.createElement('td');
+    nameCol.innerHTML = person.name;
+    ageCol.innerHTML = person.age;
+    table.appendChild(row);
+    row.appendChild(nameCol);
+    row.appendChild(ageCol);
     console.log(person);
 }
+
+
